aaea2a5b7300df18741377570867d12f25ac6d31
gdr19 <delregnogerardo5@gmail.com>
2026-06-16 16:13:38 +0200

    add:aggiunta CatalogoServlet

---
+++ Workspace/SeicenTools/src/main/java/CatalogoServlet.java
@@ -0,0 +1,19 @@
+import it.unisa.seicentools.models.Prodotto;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CatalogoServlet extends HttpServlet {
+
+    @WebServlet(name="CatalogoServlet", value="7catalogo")
+    private void doGet(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        List<Prodotto> catalogo = (List<Prodotto>) session.getAttribute("catalogo");
+    }
+}
